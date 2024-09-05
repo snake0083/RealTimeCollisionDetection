f8dabcf2affd0ddecf4bf11e809272120872d507
snakezheng <snakezheng@tencent.com>
2024-09-05 15:38:27 +0800

    添加项目文件。

---
+++ RealTimeCollisionDetection/RealTimeCollisionDetection.cpp
@@ -0,0 +1,71 @@
+﻿// RealTimeCollisionDetection.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+class Point {
+public:
+	Point();
+
+	Point operator - (const Point & rhs) const;
+public:
+	float x, y, z;
+};
+
+Point Point::operator - (const Point & rhs) const
+{
+	return Point();
+}
+
+class Vector {
+public:
+	Vector();
+	Vector( const Vector& rhs );
+	Vector( const Point& rhs );
+public:
+	float x,y,z;
+};
+
+class Sphere {
+public:
+public:
+	Point c;
+};
+
+
+// Intersects ray r = p + td, |d| = 1, with sphere s and, if intersecting,
+// returns t value of intersection and intersection point q
+int IntersectRaySphere(Point p, Vector d, Sphere s, float &t, Point &q)
+{
+	Vector m = p – s.c;
+	float b = Dot(m, d);
+	float c = Dot(m, m) – s.r * s.r;
+	// Exit if r’s origin outside s (c > 0)and r pointing away from s (b > 0)
+	if (c > 0.0f && b > 0.0f) return 0;
+	float discr = b * b – c;
+	// A negative discriminant corresponds to ray missing sphere
+	if (discr < 0.0f) return 0;
+	// Ray now found to intersect sphere, compute smallest t value of intersection
+	t = -b – Sqrt(discr);
+	// If t is negative, ray started inside sphere so clamp t to zero
+	if (t < 0.0f) t = 0.0f;
+	q = p + t * d;
+	return 1;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
